1d2c2b3da5f5dc0bb85b183a2cd8bc63664e134b
SandhyaCodes09 <vsandhya165@gmail.com>
2026-08-20 00:01:52 +0530

    update dasboard

--- frontend/src/pages/patient/Dashboard.jsx
+++ frontend/src/pages/patient/Dashboard.jsx
@@ -84,7 +84,7 @@ function Dashboard() {
             <PatientNavbar user={user} />
 
             {/* Main Content Area */}
-            <main className="flex-1 w-full max-w-7xl mx-auto p-4 sm:p-8">
+            <main className="flex-1 w-full max-w-8xl mx-auto p-4 sm:p-8">
 
                 {/* Error Banner Notification */}
                 {errorMsg && (
